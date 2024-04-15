3ef204f8eaf3f41ebf55c1c155b5c9755140cbaa
WahyuAji11 <alhafidzw1@gmail.com>
2024-04-15 22:29:31 +0800

    add cart and searh button

--- js/main.js
+++ js/main.js
@@ -2,14 +2,42 @@
 const navbarNav = document.querySelector('.navbar-nav');
 
 document.querySelector('#hamburger-menu').
-    onclick = () => {
+    onclick = (e) => {
         navbarNav.classList.toggle('active');
+        e.preventDefault();
     };
 
+// end hamburger menu
+
+const searchForm = document.querySelector('.search-form');
+const searchBox = document.querySelector('#search-box');
+
+// toogle search form
+document.querySelector('#search-button').onclick = (e) => {
+    searchForm.classList.toggle('active');
+    searchBox.focus()
+    e.preventDefault()
+}
+
+// shopping cart
+const shoppingButton = document.querySelector('#shopping-cart-button');
+const shoppingCart = document.querySelector('.shopping-cart');
+document.querySelector('#shopping-cart-button').onclick = (e) => {
+    shoppingCart.classList.toggle('active');
+    e.preventDefault();
+}
+
+// klik diluar element
 const hamburger = document.querySelector('#hamburger-menu');
+const searchButton = document.querySelector('#search-button');
 
 document.addEventListener('click', function (e) {
+    // hamburger menu
     if(!hamburger.contains(e.target) && !navbarNav.contains(e.target)) {
         navbarNav.classList.remove('active');
+    }
+    // search button
+    if(!searchButton.contains(e.target) && !searchForm.contains(e.target)) {
+        searchForm.classList.remove('active');
     }
 });
